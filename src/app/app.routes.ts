d172f31ebe52b3065e772024635c484bee91c3b6
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-05 21:43:11 +0200

    Réorganisation du code des routes pour une meilleure lisibilité

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,17 @@ import { Routes } from '@angular/router';
 import path from 'node:path';
 
 export const routes: Routes = [
-    {
-        path: '',
-        title: 'ngDuka',
-        loadComponent: () => import('./pages/home/home.component'),
-    },
-    {
-        path: 'products/:category',
-        loadComponent: () => import('./pages/products/products.component'),
-    },
-    {
-        path: 'product/:id',
-        loadComponent: () => import('./pages/product/product.component'),
-    },
+  {
+    path: '',
+    title: 'ngDuka',
+    loadComponent: () => import('./pages/home/home.component'),
+  },
+  {
+    path: 'products/:category',
+    loadComponent: () => import('./pages/products/products.component'),
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () => import('./pages/product/product.component'),
+  },
 ];
