bf6794a92027627e78d72a9d054f7ffcb54de0fb
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-05 22:49:48 +0200

    Ajout de la structure du pied de page avec des liens vers les produits

--- src/app/pages/shared/footer.component.ts
+++ src/app/pages/shared/footer.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   template: `
-    <p>
-      footer works!
-    </p>
+    <footer>
+      <div class="footer-container max-width">
+        <div class="left-container">
+          <div>
+            <p><b>PRODUITS</b></p>
+            <a routerLink="/products/electronics">Electroniques</a>  
+            <a routerLink="/products/jewelery">Bijoux</a>  
+            <a routerLink="/products/men's clothing">Vêtements pour hommes</a>
+            <a routerLink="/products/women's clothing">Vêtements pour femmes</a>  
+          </div>
+        </div>
+
+        <div class="rigth-container"></div>
+      </div>
+    </footer>
   `,
   styles: ``
 })
