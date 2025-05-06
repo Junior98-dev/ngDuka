65e9e13e10c119f83d9a854069e4d2b65d426881
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-06 17:23:52 +0200

    Ajout de la configuration de base pour le service ApiService avec l'API de fakestore et l'injection de HttpClient

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  API = 'https://fakestoreapi.com';
+  http = inject(HttpClient); 
 
-  constructor() { }
+
 }
