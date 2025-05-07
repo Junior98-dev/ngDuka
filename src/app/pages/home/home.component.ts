dfe64454b1989de9a30ecb13656dd0ce16aa0acb
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-07 20:32:51 +0200

    Ajout des composants ProductList pour afficher les produits par catégorie dans le composant Home.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,7 +10,17 @@ import { ProductListComponent } from "../products/product-list/product-list.comp
       <h3>Une boutique en ligne pour demo http client en Angular</h3>
       <input placeholder="Recherche dans ngSoko" type="text" />
     </section>
-    <app-product-list/>
+    <br/> 
+    <app-product-list sectionTitle="Electroniques" query="electronics" [queryLimitCount]="4"/>
+    <br/> 
+    <app-product-list sectionTitle="Bijoux" query="jewelery" [queryLimitCount]="4"/>
+    <br/> 
+    <app-product-list sectionTitle="Vêtements pour hommes" query="men's clothing" [queryLimitCount]="4"/>
+    <br/> 
+    <app-product-list sectionTitle="Vêtements pour femmes" query="women's clothing" [queryLimitCount]="4"/>
+
+    <br/>
+    <br/>
   `,
   styles: `
     .hero-container {
