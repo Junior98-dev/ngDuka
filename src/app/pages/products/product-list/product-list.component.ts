0280677573136e9c644244d6409f816b5691b54b
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-07 20:42:12 +0200

    Ajout de la gestion de la désinscription des abonnements dans le composant ProductList.

--- src/app/pages/products/product-list/product-list.component.ts
+++ src/app/pages/products/product-list/product-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../../../core/models/product.model';
 import { ApiService } from '../../../core/services/api.service';
 import { RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -9,15 +10,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit , OnDestroy{
   products?: Product[];
   api = inject(ApiService);
   sectionTitle = input.required<string>();
   query = input.required<string>();
   queryLimitCount = input<number>();
+  productsSub?: Subscription;
   ngOnInit(): void {
-    this.api.getProductsByCategory(this.query(), this.queryLimitCount()).subscribe((products)=>{
-      this.products = products;
-    });
+    this.productsSub = this.api
+      .getProductsByCategory(this.query(), this.queryLimitCount())
+      .subscribe((products) => {
+        this.products = products;
+      });
+  }
+  ngOnDestroy(): void {
+    this.productsSub?.unsubscribe();
   }
 }
