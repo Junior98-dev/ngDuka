6cf93c25e3c47c92c7dab894721410b21c596951
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-07 16:56:44 +0200

    Ajout de la logique pour récupérer et afficher les produits par catégorie dans le composant ProductList

--- src/app/pages/products/product-list/product-list.component.ts
+++ src/app/pages/products/product-list/product-list.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Product } from '../../../core/models/product.model';
+import { ApiService } from '../../../core/services/api.service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products?: Product[];
+  api = inject(ApiService);
 
+  ngOnInit(): void {
+    this.api.getProductsByCategory('electronics').subscribe((products)=>{
+      this.products = products;
+    });
+  }
 }
