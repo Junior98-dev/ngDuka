5a167954ed4ca2449a28765147be44d345dbf51f
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-09 01:30:34 +0200

    Formatage du code dans le service API pour améliorer la lisibilité

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -3,14 +3,20 @@ import { inject, Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   API = 'https://fakestoreapi.com';
-  http = inject(HttpClient); 
+  http = inject(HttpClient);
 
   getProductsByCategory(category: string, limitCount?: number) {
     const queryParams = limitCount ? `?limit=${limitCount}` : '';
-    return this.http.get<Product[]>(`${this.API}/products/category/${category}${queryParams}`);
+    return this.http.get<Product[]>(
+      `${this.API}/products/category/${category}${queryParams}`
+    );
   }
+
+  getProducts = () => {
+    return this.http.get<Product[]>(`${this.API}/products/`);
+  };
 }
