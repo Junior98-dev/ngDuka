3023da2d1b701f61e5b87d3a48e76339a4163658
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-09 19:21:19 +0200

    Ajout de la gestion de la désinscription dans le composant ProductComponent pour éviter les fuites de mémoire.

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { Product } from '../../core/models/product.model';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
 import { ApiService } from '../../core/services/api.service';
 import { Title } from '@angular/platform-browser';
 
@@ -11,20 +11,25 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './product.component.html',
   styles: ``,
 })
-export default class ProductComponent implements OnInit {
+export default class ProductComponent implements OnInit, OnDestroy {
   product?: Product;
   loading = signal(true);
   route = inject(ActivatedRoute);
   api = inject(ApiService);
   title = inject(Title);
+  routeSub?: Subscription;
 
   ngOnInit(): void {
-    this.route.params
+    this.routeSub = this.route.params
       .pipe(switchMap((params) => this.api.getProductById(params['id'])))
       .subscribe((product) => {
         this.product = product;
         this.title.setTitle(`${product.title} - ngDuka`);
-        this.loading.set(false);       
+        this.loading.set(false);
       });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub?.unsubscribe();
   }
 }
