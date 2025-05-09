940ffe8b8f6805e0aad6bc512fde9c9cdc0cd0ec
Junior98-dev <kounkouddorianjunior@gmail.com>
2025-05-09 19:12:17 +0200

    Ajout de la logique de récupération d'un produit par son identifiant dans le composant ProductComponent.

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Product } from '../../core/models/product.model';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { ApiService } from '../../core/services/api.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product',
   imports: [],
   templateUrl: './product.component.html',
-  styles: ``
+  styles: ``,
 })
-export default class ProductComponent {
+export default class ProductComponent implements OnInit {
+  product?: Product;
+  loading = signal(true);
+  route = inject(ActivatedRoute);
+  api = inject(ApiService);
+  title = inject(Title);
 
+  ngOnInit(): void {
+    this.route.params
+      .pipe(switchMap((params) => this.api.getProductById(params['id'])))
+      .subscribe((product) => {
+        this.product = product;
+        this.title.setTitle(`${product.title} - ngDuka`);
+        this.loading.set(false);       
+      });
+  }
 }
